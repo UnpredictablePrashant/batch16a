3ebea961f8c6b1b9ed19f47a772639bfd03b7065
Riyaz Sayyad <code3alphadown@gmail.com>
2026-02-08 12:29:19 +0530

    minor change

--- app.py
+++ app.py
@@ -5,15 +5,4 @@ print("Prashant")
 a = 6
 
 for i in range(1, 11):
-    print(a, "x", i, "=", a*i)
-    print(f"{a} x {i} = {a*i}")
-    print(f"{a} x {i} = {a*i:2d}")
-    print(f"{a} x {i} = {a*i:02d}")
-    print(f"{a} x {i} = {a*i:03d}")
-    print(f"{a} x {i} = {a*i:04d}")
-    print(f"{a} x {i} = {a*i:05d}")
-    print(f"{a} x {i} = {a*i:06d}")
-    print(f"{a} x {i} = {a*i:07d}")
-    print(f"{a} x {i} = {a*i:08d}")
-    print(f"{a} x {i} = {a*i:09d}")
     print(f"{a} x {i} = {a*i:10d}")
